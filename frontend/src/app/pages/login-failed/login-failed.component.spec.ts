f901277bf9ca272a72bfdfde5532d0cf1bd91d81
Jesus Cruz <jesus.cruz@unosquare.com>
2022-10-18 16:18:58 -0500

    test: fix LoginFailedComponent tests

--- frontend/src/app/pages/login-failed/login-failed.component.spec.ts
+++ frontend/src/app/pages/login-failed/login-failed.component.spec.ts
@@ -1,23 +1,35 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { LoginFailedComponent } from './login-failed.component';
+
+const LOGIN_FAILED_MSG = 'Login failed. Please try again.';
 
 describe('LoginFailedComponent', () => {
   let component: LoginFailedComponent;
   let fixture: ComponentFixture<LoginFailedComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFailedComponent ]
+      declarations: [LoginFailedComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginFailedComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display not found message', () => {
+    const msg = el.query(By.css('p'));
+    expect(msg.nativeElement.textContent).toBe(LOGIN_FAILED_MSG);
+  });
+
 });
