624d41ba25a954a0d4656128356abc72d1662966
Jesus Cruz <jesus.cruz@unosquare.com>
2022-10-18 16:18:17 -0500

    test: fix UsersComponent tests

--- frontend/src/app/pages/users/users.component.spec.ts
+++ frontend/src/app/pages/users/users.component.spec.ts
@@ -1,16 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { IUser } from 'src/app/interfaces/iuser';
+import { TypicodeService } from 'src/app/services/typicode.service';
 import { UsersComponent } from './users.component';
+
+const MOCK_USERS: Partial<IUser>[] = [
+  { id: 1, name: 'name1 ap1 ap2', email: 'name1@mail.com', username: 'name1_ap1', website: 'www.name1.com' },
+  { id: 2, name: 'name2 ap1 ap2', email: 'name2@mail.com', username: 'name2_ap1', website: 'www.name2.com' },
+  { id: 3, name: 'name3 ap1 ap2', email: 'name3@mail.com', username: 'name3_ap1', website: 'www.name3.com' },
+];
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
+  let typicodeServiceSpy: any;
 
   beforeEach(async () => {
+
+    typicodeServiceSpy = jasmine.createSpyObj<TypicodeService>('TypicodeService', ['getAllUsers'])
+    typicodeServiceSpy.getAllUsers.and.returnValue(of(MOCK_USERS));
+
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        UsersComponent
+      ],
+      providers: [
+        { provide: TypicodeService, useValue: typicodeServiceSpy },
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
@@ -20,4 +37,9 @@ describe('UsersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call getAllUsers on init', () => {
+    expect(typicodeServiceSpy.getAllUsers.calls.count()).toBe(1);
+  });
+
 });
