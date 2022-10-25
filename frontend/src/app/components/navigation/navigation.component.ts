6aa712cf7289499ee53195885e125d3a318828a9
Jesus Cruz <jesus.cruz@unosquare.com>
2022-10-24 20:26:31 -0500

    feat: add logout and login buttons with user name

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
+
+import { AccountInfo } from '@azure/msal-common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  account: AccountInfo | undefined | null;
+
+  constructor(public msalSrv: MsalService,
+    private msalBroadcastSrv: MsalBroadcastService) { }
 
   ngOnInit(): void {
+    this.msalBroadcastSrv.msalSubject$.subscribe(msg => {
+      setTimeout(() => {
+        const accounts = this.msalSrv.instance?.getAllAccounts();
+        this.account = accounts?.length > 0 ? accounts[0] : null;
+      }, 0);
+    });
+  }
+
+  login() {
+    this.msalSrv.loginRedirect();
+  }
+
+  logout() {
+    this.msalSrv.logout();
   }
 
 }
