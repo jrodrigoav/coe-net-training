c6427b6a8d36ce613cc39822b54754527807f2ac
raul.reyes <raul.reyes@unosquare>
2022-11-25 09:20:01 -0600

    changes requested, move code to new service file.

--- backend/UnicornRewards.API/Controllers/UserAlbumController.cs
+++ backend/UnicornRewards.API/Controllers/UserAlbumController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Linq;
 using UnicornRewards.API.Models;
+using UnicornRewards.API.Services;
 
 namespace UnicornRewards.API.Controllers
 {
@@ -13,65 +14,9 @@ namespace UnicornRewards.API.Controllers
         [Authorize, HttpGet("getAllAlbums/{message}")]
         public async Task<IActionResult> GetUserAlbum()
         {
-            var usersList = await GetAllUsers();
-            var albumsList = await GetAllAlbums();
-            if (string.IsNullOrEmpty(usersList) || string.IsNullOrEmpty(albumsList) ) 
-            {
-                var res = new UserAlbum();
-                return Ok(res);
-            }
-            var users = JsonConvert.DeserializeObject<List<User>>(usersList);
-            var albums = JsonConvert.DeserializeObject<List<Album>>(albumsList);
-
-            var result = from u in users
-                         join a in albums on u.Id equals a.UserId into ua
-                         select new UserAlbum
-                         {
-                             Id = u.Id,
-                             Name = u.Name,
-                             Email= u.Email,
-                             AlbumName = ua.Where(x => x.UserId == u.Id).Select(y => y.Title).ToList(),
-                         };
-            
+            var usersAlbums = new UserAlbumService();
+            var result = await usersAlbums.GetAllUserAlbums();            
             return Ok(result);
-        }
-
-        private async Task<string> GetAllUsers()
-        {
-            try
-            {
-                using var client = new HttpClient();
-                var content = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
-                var result = await content.Content.ReadAsStringAsync();
-                if (content.StatusCode.ToString() != "OK")
-                {
-                    return string.Empty;
-                }
-                return result;
-            }
-            catch (Exception)
-            {
-                return string.Empty;
-            }
-        }
-
-        private async Task<string> GetAllAlbums()
-        {
-            try
-            {
-                using var client = new HttpClient();
-                var content = await client.GetAsync("https://jsonplaceholder.typicode.com/albums");
-                var result = await content.Content.ReadAsStringAsync();
-                if (content.StatusCode.ToString() != "OK")
-                {
-                    return string.Empty;
-                }
-                return result;
-            }
-            catch (Exception)
-            {
-                return string.Empty;
-            }
         }
     }
 }
---
+++ backend/UnicornRewards.API/Services/UserAlbumService.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.Json;
+using UnicornRewards.API.Models;
+
+namespace UnicornRewards.API.Services
+{
+    public class UserAlbumService
+    {
+        public async Task<List<UserAlbum>> GetAllUserAlbums()
+        {
+            var usersList = await GetAllUsers();
+            var albumsList = await GetAllAlbums();
+            if (string.IsNullOrEmpty(usersList) || string.IsNullOrEmpty(albumsList))
+            {
+                var res = new List<UserAlbum>();
+                return res;
+            }
+            var users = JsonSerializer.Deserialize<List<User>>(usersList, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            var albums = JsonSerializer.Deserialize<List<Album>>(albumsList, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            var result = from u in users
+                         join a in albums on u.Id equals a.UserId into ua
+                         select new UserAlbum
+                         {
+                             Id = u.Id,
+                             Name = u.Name,
+                             Email = u.Email,
+                             AlbumName = ua.Where(x => x.UserId == u.Id).Select(y => y.Title).ToList(),
+                         };
+
+            return result.ToList();
+        }
+
+        private async Task<string> GetAllUsers()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                var content = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+                var result = await content.Content.ReadAsStringAsync();
+                if (content.StatusCode.ToString() != "OK")
+                {
+                    return string.Empty;
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        private async Task<string> GetAllAlbums()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                var content = await client.GetAsync("https://jsonplaceholder.typicode.com/albums");
+                var result = await content.Content.ReadAsStringAsync();
+                if (content.StatusCode.ToString() != "OK")
+                {
+                    return string.Empty;
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
